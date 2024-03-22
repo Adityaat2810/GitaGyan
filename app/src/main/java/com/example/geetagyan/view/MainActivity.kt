6e57305abe9dec5ce3cd1a31c78bd32d50959f28
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-03-22 15:51:56 +0530

    setup the basic project

---
+++ app/src/main/java/com/example/geetagyan/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.geetagyan.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.geetagyan.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
