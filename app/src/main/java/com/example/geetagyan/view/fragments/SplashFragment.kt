488ee4fdb8745280bd12769c2a1e19ed1933b628
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-03-22 16:25:38 +0530

    done with designing of splash screen

--- app/src/main/java/com/example/geetagyan/view/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/geetagyan/view/fragments/SplashFragment.kt
@@ -1,10 +1,16 @@
 package com.example.geetagyan.view.fragments
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
+import androidx.navigation.fragment.findNavController
 import com.example.geetagyan.R
 
 class SplashFragment : Fragment() {
@@ -15,7 +21,24 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        changeStatusBarColor()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        },1500)
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    private fun changeStatusBarColor() {
+
+        val window = activity?.window
+        window?.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window?.statusBarColor = ContextCompat.getColor(requireContext(), R.color.splash)
+        if (window != null) {
+            WindowCompat.getInsetsController(window, window.decorView).apply {
+                isAppearanceLightStatusBars = true
+            }
+        }
     }
 
 
