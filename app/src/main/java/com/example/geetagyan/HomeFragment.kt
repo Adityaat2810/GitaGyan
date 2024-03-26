7322a0c6b2cd1a899511d754e262be45114e158f
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-03-26 13:28:12 +0530

    basic design of home page UI

--- app/src/main/java/com/example/geetagyan/HomeFragment.kt
+++ app/src/main/java/com/example/geetagyan/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 
 
 class HomeFragment : Fragment() {
@@ -15,8 +18,22 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        changeStatusBarColor()
+
 
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+
+    private fun changeStatusBarColor() {
+
+        val window = activity?.window
+        window?.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window?.statusBarColor = ContextCompat.getColor(requireContext(), R.color.splash)
+        if (window != null) {
+            WindowCompat.getInsetsController(window, window.decorView).apply {
+                isAppearanceLightStatusBars = true
+            }
+        }
+    }
 }
