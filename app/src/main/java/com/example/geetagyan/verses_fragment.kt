9a43b81152b12c0328b8414c54d46849bcad98f2
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-04-09 17:51:08 +0530

    almost done with verse fragment

--- app/src/main/java/com/example/geetagyan/verses_fragment.kt
+++ app/src/main/java/com/example/geetagyan/verses_fragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.example.geetagyan.databinding.FragmentVersesFragmentBinding
+import com.example.geetagyan.view.adapters.AdapterVerses
 import com.example.geetagyan.viewmodel.MainViewModel
 import kotlinx.coroutines.launch
 
@@ -16,6 +17,9 @@ class verses_fragment : Fragment() {
 
     private lateinit var binding:FragmentVersesFragmentBinding
     private val viewModel:MainViewModel by viewModels()
+    private lateinit var versesAdapter:AdapterVerses
+    private var chapterNumber  = 0
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,13 +27,31 @@ class verses_fragment : Fragment() {
     ): View? {
         binding = FragmentVersesFragmentBinding.inflate(layoutInflater)
         getAndSetChapterDetails()
-        getAllVerses()
+        onReadMoreClick()
+        checkInternetConnectivity()
         return binding.root
+    }
+
+    private fun onReadMoreClick() {
+        var isExpanded = false
+        binding.readMore.setOnClickListener {
+            if(!isExpanded){
+                binding.chDes.maxLines =50
+                isExpanded =true
+                binding.readMore.text ="Read less"
+            }else{
+                binding.chDes.maxLines =4
+                isExpanded =false
+                binding.readMore.text ="Read more"
+            }
+
+        }
     }
 
     private fun getAndSetChapterDetails() {
         // this line ,must remain in onCreate
         val bundle = arguments
+        chapterNumber = bundle?.getInt("chapterNumber")!!
         binding.chNumber.text = "Chapter ${bundle?.getInt("chapterNumber")}"
         binding.chName.text = bundle?.getString("chapterTitle")
         binding.chDes.text = bundle?.getString("chapterDes")
@@ -39,13 +61,49 @@ class verses_fragment : Fragment() {
 
     private fun getAllVerses() {
         lifecycleScope.launch {
-            viewModel.getVerses(4).collect{
-                for(i in it ){
-                    Log.d("TAg",i.toString())
+            viewModel.getVerses(chapterNumber).collect{
+                versesAdapter = AdapterVerses()
+                binding.versesRv.adapter = versesAdapter
+                val verseList = arrayListOf<String>()
+
+                for(currentVerse in it ){
+                    for (verse in currentVerse.translations){
+                        if(verse.language == "english"){
+                            verseList.add(verse.description)
+                            break
+                        }
+                    }
                 }
+
+                versesAdapter.differ.submitList(verseList)
+                binding.shimmerLayout.visibility=View.GONE
+                binding.versesRv.visibility=View.VISIBLE
             }
         }
 
+    }
+
+    private fun checkInternetConnectivity() {
+        val networkManager =NetworkManager(requireContext())
+        networkManager.observe(viewLifecycleOwner){
+            if(it ==true){
+                binding.shimmerLayout.visibility =View.VISIBLE
+                //binding.chaptersRV.visibility =View.VISIBLE
+                getAllVerses()
+
+
+            }else{
+
+                binding.shimmerLayout.visibility =View.GONE
+                binding.versesRv.visibility =View.GONE
+
+                /*
+                * show something here like please turn your internet on
+                * */
+
+            }
+
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/geetagyan/view/adapters/AdapterVerses.kt
@@ -0,0 +1,40 @@
+package com.example.geetagyan.view.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.geetagyan.databinding.VersesItemBinding
+
+class AdapterVerses:RecyclerView.Adapter<AdapterVerses.VerseViewHolder>() {
+    class VerseViewHolder(val binding:VersesItemBinding):ViewHolder(binding.root)
+
+
+    val diffUtil =object :DiffUtil.ItemCallback<String>(){
+        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+           return oldItem == newItem
+        }
+
+    }
+
+    val differ =AsyncListDiffer(this , diffUtil)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VerseViewHolder {
+        return VerseViewHolder(VersesItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: VerseViewHolder, position: Int) {
+        val verse = differ.currentList[position]
+        holder.binding.name.text = "VERSE ${position+1}"
+        holder.binding.des.text = verse
+    }
+}
