210098ac8f1776ed13d3d6263596714365288163
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-04-13 12:13:17 +0530

    displayed saved verses list in saved-verses-fragment

--- app/src/main/java/com/example/geetagyan/SavedVersesFragment.kt
+++ app/src/main/java/com/example/geetagyan/SavedVersesFragment.kt
@@ -5,55 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.geetagyan.databinding.FragmentSavedVersesBinding
+import com.example.geetagyan.view.adapters.AdapterVerses
+import com.example.geetagyan.viewmodel.MainViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SavedVersesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SavedVersesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentSavedVersesBinding
+    private val viewModel:MainViewModel by viewModels()
+    private lateinit var adapterVerse:AdapterVerses
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_verses, container, false)
+        binding = FragmentSavedVersesBinding.inflate(layoutInflater)
+        getVersesFromRoom()
+        return binding.root
+
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SavedVersesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SavedVersesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun getVersesFromRoom() {
+        viewModel.getSavedVerses().observe(viewLifecycleOwner){
+            val verseList = arrayListOf<String>()
+            for (i in it){
+                verseList.add(i.translations[0].description)
             }
+
+            if(verseList.isEmpty()){
+                binding.shimmerLayout.visibility =View.GONE
+            }
+
+            adapterVerse = AdapterVerses(::onVerseItemClicked,true)
+            binding.versesRv.adapter = adapterVerse
+            adapterVerse.differ.submitList(verseList)
+            binding.shimmerLayout.visibility = View.GONE
+            binding.versesRv.visibility =View.VISIBLE
+        }
+
     }
+
+
+    private fun onVerseItemClicked(verse: String, verseNumber: Int) {
+
+    }
+
 }
